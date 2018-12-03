f78ef610a25794b803a899512d7775c9b6adbad8
Tom Steele <tsteele@madisoncollege.edu>
2018-12-02 23:53:24 -0600

    Expand...
    Wow ! ! !

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, merge } from 'rxjs';
+import { Observable, empty } from 'rxjs';
+import { expand } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,16 +12,22 @@ export class SwapiService {
 
   public getListOfPlanets() {
     //console.log('getListOfPlanets()');
-    let p1 = this.httpSvc.get('https://swapi.co/api/planets/?page=1');
-    let p2 = this.httpSvc.get('https://swapi.co/api/planets/?page=2');
-    let p3 = this.httpSvc.get('https://swapi.co/api/planets/?page=3');
-    let p4 = this.httpSvc.get('https://swapi.co/api/planets/?page=4');
-    let p5 = this.httpSvc.get('https://swapi.co/api/planets/?page=5');
-    let p6 = this.httpSvc.get('https://swapi.co/api/planets/?page=6');
-    let p7 = this.httpSvc.get('https://swapi.co/api/planets/?page=7');
 
-    //return forkJoin(p1, p2, p3);
-    return merge(p1, p2, p3, p4, p5, p6, p7);
-    //return p1.combineLatest(p2);
+    // let p1 = this.httpSvc.get('https://swapi.co/api/planets/?page=1');
+    // let p2 = this.httpSvc.get('https://swapi.co/api/planets/?page=2');
+    // let p3 = this.httpSvc.get('https://swapi.co/api/planets/?page=3');
+    // let p4 = this.httpSvc.get('https://swapi.co/api/planets/?page=4');
+    // let p5 = this.httpSvc.get('https://swapi.co/api/planets/?page=5');
+    // let p6 = this.httpSvc.get('https://swapi.co/api/planets/?page=6');
+    // let p7 = this.httpSvc.get('https://swapi.co/api/planets/?page=7');
+
+    // return merge(p1, p2, p3, p4, p5, p6, p7);
+
+
+    return this.getPage('https://swapi.co/api/planets').pipe(expand((data, i) => (<any> data).next ? this.getPage((<any> data).next) : empty()))
+  }
+
+  private getPage(pageUrl) {
+    return this.httpSvc.get(pageUrl);
   }
 }
